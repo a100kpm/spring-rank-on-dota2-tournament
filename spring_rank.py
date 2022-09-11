8f4eaf73483e9faae42063d1e25d1dbe7ff7d7fc
a100kpm <iannis3toussaint@gmail.com>
2022-09-11 15:51:08 +0200

    Update spring_rank.py

--- spring_rank.py
+++ spring_rank.py
@@ -10,6 +10,10 @@ import scipy
 from math import exp
 
 def create_A_matrix(data,nbr_team):
+# data -> raw data of the form winner,looser,value_win
+# nbr_team -> number of team
+
+# output the A matrix and the nodes associated needed for spring rank computation
     A=np.zeros([nbr_team,nbr_team])
     nodes={}
     compteur=0
@@ -25,6 +29,11 @@ def create_A_matrix(data,nbr_team):
     return A,nodes
 
 def create_A_matrix_with_nodes(data,nbr_team,nodes):
+# data -> raw data of the form winner,looser,value_win
+# nbr_team -> number of team
+# nodes -> nodes of the A matrix
+
+# output the A matrix needed for spring rank computation
     A=np.zeros([nbr_team,nbr_team])
     for row in data:
         A[nodes[row[0]],nodes[row[1]]]+=int(row[2])
@@ -66,6 +75,12 @@ def linear_solver(A, B):
     return sol
 
 def ranking(A,nbr_team,alpha=0):
+# A -> A matrix for spring rank
+# nbr_team -> number of team
+# alpha -> parameter of spring rank, if spring rank cannot solve with alpha = 0 consider using a small value
+#usually, smaller (>0) is better
+
+# output the order of the team according to spring rank
     l0=1.
     l1=1.
     A, B = build_from_dense(A,nbr_team, alpha, l0, l1)
@@ -75,7 +90,11 @@ def ranking(A,nbr_team,alpha=0):
     return rank
 
 def proba_win(score1,score2,beta=1,rounding=-1):
-#    calculate probability that team with score1 win over team with score 2
+# score1 and score2 -> the spring rank value of team1 and team2
+# beta -> the temperature coefficient, need to be calculated before hand
+# rounding -> how many digit of rounding, default -1 imply no rounding
+    
+# output the probability of winning of team1 team2
     result=1/(1+exp(-beta*(score1-score2)))
     if rounding==-1:
         return result
@@ -83,6 +102,12 @@ def proba_win(score1,score2,beta=1,rounding=-1):
         return round(1/(1+exp(-beta*(score1-score2))),rounding)
 
 def spring_rank_hero(data_tour,nbr_hero,dico):
+# data_tour -> data of the tournament generated with create_hero_data_for_springrank
+#consist of a list of element of the form [hero_winner,hero_looser,value]
+# nbr_hero -> number of heroes
+# dico -> dictionnary of heroes
+
+#output the spring rank computation of the heroes of the tournament
     A,nodes=create_A_matrix(data_tour,nbr_hero)
     rank=ranking(A,nbr_hero,alpha=0)
     rank-=min(rank)
@@ -109,6 +134,14 @@ def spring_rank_hero(data_tour,nbr_hero,dico):
     return name_score,beta
 
 def extract_beta(rank,nbr_hero,A,start=0,end=10,step=100,power=2):
+# rank -> ranking of the heroes according to spring rank
+# nbr_hero -> number of heroes
+# A -> spring rank A matrix
+# start and end -> boundary to search for beta
+# step -> how many time the boundary is divided for the discret search of beta
+# power -> weight on the distance for the error (default is square distance)
+
+# output the best beta coefficient for lowest error on prediction
     A_test=np.ones([nbr_hero,nbr_hero])*-1
     for i in range(nbr_hero):
         for j in range(nbr_hero):
@@ -133,7 +166,11 @@ def extract_beta(rank,nbr_hero,A,start=0,end=10,step=100,power=2):
     return beta
 
 def link_ranking_different_beta(rank_beta,rank,target_beta):
-    # rank is either the solution from spring_rank ranking or the ordered score with name
+# rank_beta -> beta value of the ranking rank
+# rank -> either the solution from spring_rank ranking or the ordered score with name
+# target_beta -> forcing rank value to match best prediction with target_beta instead of rank_beta
+
+# output modified rank so that best prediction is done with target_beta instead of his rank_beta
     multiplier=rank_beta/target_beta
     if len(np.shape(rank))==1:
         return rank*multiplier
@@ -141,14 +178,8 @@ def link_ranking_different_beta(rank_beta,rank,target_beta):
     return rank
 
 def see_gain(changement_val,beta):
-    return 1/(1+exp(-beta*changement_val))-0.5
+# changement_val -> difference of spring rank value between team1 and team2
+# beta -> beta value of the ranking where changement_val has been computed
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
+# output the gain (positive) or loss (negative) of winrate of team1 over team2 compared to the 50/50 winrate
+    return 1/(1+exp(-beta*changement_val))-0.5
