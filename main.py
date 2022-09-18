9edc3459dc375bc1154dae52ce4e00e398117232
a100kpm <iannis3toussaint@gmail.com>
2022-09-18 23:58:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,17 +23,16 @@ find_function('find_function')
 # =============================================================================
 
 
+# =============================================================================
+# # First plot
+# df1,_,beta1=compare_inside_tournament('tournament.csv')
+# plot_hero_ranking_distribution_per_game(df1)
+# =============================================================================
+
+
 
 # =============================================================================
-# First plot
-# # df1,_,beta1=compare_inside_tournament('tournament.csv')
-# # plot_hero_ranking_distribution_per_game(df1)
-# =============================================================================
-
-
-
-# =============================================================================
-# Second plot
+# # # Second plot
 # # df2,beta2=compare_between_tournament(tournament=['tournament.csv','tournament2.csv'],dynamic_min_game=False)
 # # plot_hero_ranking_between_tournament(df2)
 # =============================================================================
@@ -41,10 +40,18 @@ find_function('find_function')
 
 
 # =============================================================================
-# Third plot
-# Warning the computation of hero_rank_gain_per_team() can be very long as it will
-# easily require to compute the spring rank ranking over 500 times. This can take 
-# over 1h to compute
+# # # Third plot
+# # # Warning the computation of hero_rank_gain_per_team() can be very long as it will
+# # # easily require to compute the spring rank ranking over 500 times. This can take 
+# # # over 1h to compute
 # # df_global,beta,team_list=hero_rank_gain_per_team(path='tournament.csv')
 # # plot_gain_all_team(df_global,team_list,beta)
+# =============================================================================
+
+
+# =============================================================================
+# team ranking
+# ---> 
+# rank,beta=ranking_team(path='tournament.csv',start_beta=0,end_beta=10)
+# print(proba_win(rank[0][1],rank[1][1],beta))
 # =============================================================================
